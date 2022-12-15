a0fa70be98aa467e5f1164aaad5848289d0c7283
llblch <mustqwert@gmail.com>
2022-12-15 14:18:01 -0800

    C - Stacks, Queues - LIFO, FIFO

---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - function that prints the top
+ * @head: stack head
+ * @counter: line_number
+ *
+ * Return: returns nothing
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
